ac0780d9166644aab5d497fd961bf7b3cf81a623
Miten Chauhan <miten@chauhan.nz>
2022-09-25 11:58:31 +1300

    Add 'back to top button'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 
 import { Header } from '../components/header'
+import { MotionImage } from '../components/motion-image'
 import {
   About,
   Contact,
@@ -43,6 +45,25 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <Contact />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer z-30">
+          <div className="flex items-center justify-center">
+            <MotionImage
+              imageProps={{
+                src: 'https://mitenchauhan.com/_nuxt/img/122f864.png',
+                layout: 'fill',
+                objectFit: 'cover',
+                priority: true,
+              }}
+              motionProps={{
+                className:
+                  'h-10 w-10 rounded-full relative overflow-hidden filter grayscale hover:grayscale-0 cursor-pointer',
+              }}
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   )
 }
