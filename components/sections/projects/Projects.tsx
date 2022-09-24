1a68876a1cffe639383a94faf1daacacb3475f9b
Miten Chauhan <miten@chauhan.nz>
2022-09-25 11:48:16 +1300

    Implement tailwind scrollbars

--- components/sections/projects/Projects.tsx
+++ components/sections/projects/Projects.tsx
@@ -19,7 +19,7 @@ const Projects = ({}: Props) => {
         Projects
       </h3>
 
-      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-10">
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-10 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {projects.map((project, i) => (
           <motion.div
             key={project}
