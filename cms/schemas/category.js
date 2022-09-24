3fce45da724baf9b4173882bad1e829199b3a96d
Miten Chauhan <miten@chauhan.nz>
2022-09-25 12:18:11 +1300

    Integrate sanity cms

---
+++ cms/schemas/category.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+  ],
+}
