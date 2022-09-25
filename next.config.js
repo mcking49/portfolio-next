d82788f623c981a440f0bbbaa7c53413a232e16a
Miten Chauhan <miten@chauhan.nz>
2022-09-25 18:56:53 +1300

    Implement hero

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['chauhan.nz', 'mitenchauhan.com'],
+    domains: ['chauhan.nz', 'mitenchauhan.com', 'cdn.sanity.io'],
   },
 }
 
