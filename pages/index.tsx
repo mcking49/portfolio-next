ba3af0c4f62667cd5ed8989e4ae8fe8d533a53a7
Miten Chauhan <miten@chauhan.nz>
2022-09-26 09:52:31 +1300

    Connect projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ const Home: NextPage<Props> = ({
       </section>
 
       <section id="projects" className="snap-start">
-        <Projects />
+        <Projects projects={projects} />
       </section>
 
       <section id="contact" className="snap-start">
